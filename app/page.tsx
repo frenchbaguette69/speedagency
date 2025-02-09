85e061afc6f4c8156b0f590d12912113487ef2c6
frenchbaguette69 <jonkovoorjou@gmail.com>
2025-02-09 21:27:08 +0100

    new features seo

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,65 @@ import Image from "next/image";
 
 
 export default function Home() {
+
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    "name": "Next Website",
+    "url": "https://nextwebsite.nl",
+    "description": "Wil je een professionele website laten maken? Next Website ontwikkelt razend snelle, SEO-geoptimaliseerde websites op maat met Next.js.",
+    "image": "https://nextwebsite.nl/images/next-website-homepage.jpg",
+    "author": {
+      "@type": "Organization",
+      "name": "Next Website",
+      "logo": "https://nextwebsite.nl/logo.png",
+      "url": "https://nextwebsite.nl"
+    },
+    "sameAs": [
+      "https://www.facebook.com/nextwebsite",
+      "https://www.linkedin.com/company/nextwebsite",
+      "https://www.instagram.com/nextwebsite"
+    ],
+    "potentialAction": {
+      "@type": "SearchAction",
+      "target": "https://nextwebsite.nl/?s={search_term_string}",
+      "query-input": "required name=search_term_string"
+    },
+    "aggregateRating": {
+      "@type": "AggregateRating",
+      "ratingValue": "4.9",
+      "reviewCount": "120"
+    },
+    "review": [
+      {
+        "@type": "Review",
+        "author": {
+          "@type": "Person",
+          "name": "Jan de Vries"
+        },
+        "reviewRating": {
+          "@type": "Rating",
+          "ratingValue": "5",
+          "bestRating": "5"
+        },
+        "reviewBody": "Next Website heeft een fantastische website voor mijn bedrijf gemaakt. Super snel en SEO-vriendelijk!"
+      },
+      {
+        "@type": "Review",
+        "author": {
+          "@type": "Person",
+          "name": "Lisa van den Berg"
+        },
+        "reviewRating": {
+          "@type": "Rating",
+          "ratingValue": "4.9",
+          "bestRating": "5"
+        },
+        "reviewBody": "Professionele service en duidelijke communicatie. Mijn website draait perfect op Next.js!"
+      }
+    ]
+  };
+
   return (
     <div className="">
       <NavbarWithChildren />
