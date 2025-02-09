874b74a41be28cddfbbe14c5d902bcb787fec1d0
frenchbaguette69 <jonkovoorjou@gmail.com>
2025-02-09 21:32:44 +0100

    new logo

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -291,7 +291,7 @@ const Logo = () => {
       className="font-normal flex space-x-2 items-center text-sm mr-4  text-black px-2 py-1  relative z-20"
     >
       <Image
-        src="/logospeedagency.png"
+        src="/nextwebsitelogo.png"
         alt="logo"
         width={30}
         height={30}
